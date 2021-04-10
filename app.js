62d2945ad4b22e2364d2fd3baee21785ac91b38a
Yusha <yushaushaibu1@gmail.com>
2021-04-10 16:35:55 +0000

    updated the project files with newer features.

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-document.getElementById('favcolor1').addEventListener('input', getFavColor1)
-document.getElementById('favcolor2').addEventListener('input', getFavColor2);
-document.getElementById('gradient-background');
-document.querySelector('.output');
+const favColor1 = document.getElementById('favcolor1');
+const favColor2 = document.getElementById('favcolor2');
+const gradientBackground = document.getElementById('gradient-background')
 
-function getFavColor1() {
-    console.log(favcolor1.value);
+
+function changeColor() {
+    gradientBackground.style.background = "linear-gradient(to right, " + favColor1.value + ", " + favColor2.value +")";
+    
+    let subtitle;
+    subtitle =`<p>linear-gradient(to right,  ${favColor1.value}, ${favColor2.value});</p>`;
+    const output = document.querySelector('.output').innerHTML = subtitle;
+
+    
 }
+
+favColor1.addEventListener('input', changeColor);
+favColor2.addEventListener('input', changeColor);
+
+
+
+
+
