f032dcd7b3438ce706cc400cd8ce4634749abe98
Yusha <yushaushaibu1@gmail.com>
2021-04-10 14:45:26 +0000

    updated files

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('favcolor1').addEventListener('input', getFavColor1)
+document.getElementById('favcolor2').addEventListener('input', getFavColor2);
+document.getElementById('gradient-background');
+document.querySelector('.output');
+
+function getFavColor1() {
+    console.log(favcolor1.value);
+}
